f64e7e727d82990dca8ae4518cfc09df08ebeca3
Eli R <erapkin_be@dev8.devcodecamp.inc>
2015-10-22 16:50:34 -0500

    first commit, Server

---
+++ Server/Server/ClientHandler.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Server
+{
+    public class ClientHandler
+    {
+
+        TcpClient ClientSocket;
+        bool ContinueProcess = false;
+        Thread ClientThread;
+        
+
+        public ClientHandler(TcpClient ClientSocket)
+        {
+            this.ClientSocket = ClientSocket;
+        }
+
+        public void Start()
+        {
+            ContinueProcess = true;
+            ClientThread = new Thread(new ThreadStart(Process));
+            ClientThread.Start();
+        }
+
+        private void Process()
+        {
+            string dataFromClient = null;
+
+            byte[] bytes;
+
+            if (ClientSocket != null)
+            {
+                NetworkStream networkStream = ClientSocket.GetStream();
+                ClientSocket.ReceiveTimeout = 100; //miliseconds
+
+                while (ContinueProcess)
+                {
+                    bytes = new byte[ClientSocket.ReceiveBufferSize];
+                    try
+                    {
+                        int BytesRead = networkStream.Read(bytes, 0, (int)ClientSocket.ReceiveBufferSize);
+                        if (BytesRead > 0)
+                        {
+                            dataFromClient = Encoding.ASCII.GetString(bytes, 0, BytesRead);
+
+                            foreach (TcpClient client in SynchronousSocketListener.ClientList)
+                            {
+                                if (client != this.ClientSocket)
+                                {
+                                    NetworkStream responseStream = client.GetStream();
+                                    responseStream.Write(bytes, 0, bytes.Length);
+                                }
+                            }
+                            Console.WriteLine("Text received : {0}", dataFromClient);
+            
+                           // byte[] sendBytes = Encoding.ASCII.GetBytes(dataFromClient);
+                           // networkStream.Write(sendBytes, 0, sendBytes.Length);
+
+                            if (dataFromClient == "quit") break;
+
+                        }
+                    }
+                    catch (IOException) { } 
+                    catch (SocketException)
+                    {
+                        Console.WriteLine("Conection is broken!");
+                        break;
+                    }
+                    Thread.Sleep(200);
+                } 
+                networkStream.Close();
+                ClientSocket.Close();
+            }
+        }  
+
+        public void Stop()
+        {
+            ContinueProcess = false;
+            if (ClientThread != null && ClientThread.IsAlive)
+                ClientThread.Join();
+        }
+
+        public bool IsAlive
+        {
+            get
+            {
+                return (ClientThread != null && ClientThread.IsAlive);
+            }
+        }
+
+    } 
+}
---
+++ Server/Server/SynchronousSocketListener.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class SynchronousSocketListener
+    {
+
+        private const int portNum = 9999;
+        private static ArrayList ClientSockets;
+        private static bool ContinueReclaim = true;
+        private static Thread ThreadReclaim;
+        public static HashSet<TcpClient> ClientList = new HashSet<TcpClient>() { };
+
+        public static void StartListening()
+        {
+
+            ClientSockets = new ArrayList();
+
+            ThreadReclaim = new Thread(new ThreadStart(Reclaim));
+            ThreadReclaim.Start();
+
+            TcpListener listener = new TcpListener(IPAddress.Any, portNum);
+            try
+               {
+                listener.Start();
+
+                int Cycle = 3;
+                int ClientNbr = 0;
+
+                Console.WriteLine("Waiting for a connection...");
+                while (Cycle > 0)
+                {
+
+                    TcpClient handler = listener.AcceptTcpClient();
+
+                    if (handler != null)
+                    {
+                        Console.WriteLine("Client#{0} accepted!", ++ClientNbr);
+                        
+                        lock (ClientSockets.SyncRoot)
+                        {
+                            int i = ClientSockets.Add(new ClientHandler(handler));
+                            ((ClientHandler)ClientSockets[i]).Start();
+
+                            ClientList.Add(handler);
+
+                        }
+                        --Cycle;
+                    }
+                    else
+                        break;
+                }
+                listener.Stop();
+
+                ContinueReclaim = false;
+                ThreadReclaim.Join();
+
+                foreach (Object Client in ClientSockets)
+                {
+                    ((ClientHandler)Client).Stop();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("\nHit enter to continue...");
+            Console.Read();
+
+        }
+
+        private static void Reclaim()
+        {
+            while (ContinueReclaim)
+            {
+                lock (ClientSockets.SyncRoot)
+                {
+                    for (int x = ClientSockets.Count - 1; x >= 0; x--)
+                    {
+                        Object Client = ClientSockets[x];
+                        if (!((ClientHandler)Client).IsAlive)
+                        {
+                            ClientSockets.Remove(Client);
+                            Console.WriteLine("A client left");
+                        }
+                    }
+                }
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
